829e4faebb0992aac4145f531d8824177e64e583
NEETEPAT <NEETEPAT@NEETEPAT-T490.in.oracle.com>
2021-02-24 17:52:02 +0530

    beans added

---
+++ src/main/java/beans/ParkingPlace.java
@@ -0,0 +1,12 @@
+package beans;
+
+import constants.ParkingType;
+
+public class ParkingPlace {
+
+	private String number;
+	private boolean free;
+	private Vehicle vehicle;
+	private ParkingType type;
+
+}
---
+++ src/main/java/beans/Vehicle.java
@@ -0,0 +1,8 @@
+package beans;
+
+import constants.VehicleType;
+
+public abstract class Vehicle {
+	private String licenseNumber;
+	private  VehicleType type;
+}
